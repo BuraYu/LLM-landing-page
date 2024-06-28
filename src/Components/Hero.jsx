824490dc0b26e00788e2d6db2e0ac2702c4b7843
BuraYu <burak.yueksel@hotmail.com>
2024-06-28 17:46:36 +0200

    add Heroes Component

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import "./Hero.css";
+import headerlogo from "../images/llm-image.png";
 
 export default function Hero() {
   return (
-    <header>
-      <h1 className="hero">this is the header</h1>
+    <header className="header">
+      <div className="header--container">
+        <div className="header--title">
+          <h1>LLM Hosting</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem fugiat, rem assumenda corrupti, eaque laudantium, eum
+            eos corporis nisi nihil tempore asperiores voluptates aliquid omnis.
+            Hic excepturi quod impedit saepe.
+          </p>
+          <div className="header--email">
+            <label for="email">Enter you email adress</label>
+            <input
+              type="email"
+              id="email"
+              required
+              placeholder="JohnDoe@gmail.com"
+            ></input>
+            <button>Get startd</button>
+          </div>
+        </div>
+        <img src={headerlogo} alt="" srcset="" />
+      </div>
     </header>
   );
 }
