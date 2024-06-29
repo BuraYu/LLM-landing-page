df8d22a928bc20b1b40f0ed10853ff191d2ab100
BuraYu <burak.yueksel@hotmail.com>
2024-06-29 23:10:13 +0200

    add basic style for steps component

--- src/Components/Steps.jsx
+++ src/Components/Steps.jsx
@@ -4,37 +4,70 @@ import "./Steps.css";
 export default function Steps() {
   return (
     <div className="steps">
-      <div className="steps-step1--container">
-        <h3 className="steps-step1--header">Step 1</h3>
-        <p className="steps-step1--description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ad
-          enim quia assumenda delectus alias doloremque. Placeat, ab, eligendi
-          optio hic saepe voluptatibus eaque, necessitatibus labore odit dolor
-          in adipisci assumenda harum maiores nisi animi aspernatur molestias
-          ipsam laboriosam tempore dolore! In, autem accusantium? Nam itaque
-          velit ipsum ad fugit!
+      <div className="steps-step--container">
+        <div className="steps-step-information">
+          <h3 className="steps-step--header">Step 1</h3>
+          <p className="steps-step--description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            ad enim quia assumenda delectus alias doloremque. Placeat, ab,
+            eligendi optio hic saepe voluptatibus eaque, necessitatibus labore
+            odit dolor in adipisci assumenda harum maiores nisi animi aspernatur
+            molestias ipsam laboriosam tempore dolore! In, autem accusantium?
+            Nam itaque velit ipsum ad fugit!
+          </p>
+        </div>
+        {/* 'insert image instead of <p>' */}
+        <p className="img-placeholder">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+          deleniti nemo ex necessitatibus libero veniam quos, in, amet iure, hic
+          unde corrupti praesentium tempora autem corporis sed ab ea neque
+          voluptatibus id voluptatum molestias nesciunt. Cum natus consectetur
+          suscipit consequatur, architecto excepturi optio. Et delectus fugit
+          minus dignissimos placeat amet?
         </p>
       </div>
-      <div className="steps-step2--container">
-        <h3 className="steps-step2--header">Step 1</h3>
-        <p className="steps-step2--description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ad
-          enim quia assumenda delectus alias doloremque. Placeat, ab, eligendi
-          optio hic saepe voluptatibus eaque, necessitatibus labore odit dolor
-          in adipisci assumenda harum maiores nisi animi aspernatur molestias
-          ipsam laboriosam tempore dolore! In, autem accusantium? Nam itaque
-          velit ipsum ad fugit!
+      <div className="steps-step--container">
+        <div className="steps-step-information">
+          <h3 className="steps-step--header">Step 1</h3>
+          <p className="steps-step--description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            ad enim quia assumenda delectus alias doloremque. Placeat, ab,
+            eligendi optio hic saepe voluptatibus eaque, necessitatibus labore
+            odit dolor in adipisci assumenda harum maiores nisi animi aspernatur
+            molestias ipsam laboriosam tempore dolore! In, autem accusantium?
+            Nam itaque velit ipsum ad fugit!
+          </p>
+        </div>
+        {/* 'insert image instead of <p>' */}
+        <p className="img-placeholder">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+          deleniti nemo ex necessitatibus libero veniam quos, in, amet iure, hic
+          unde corrupti praesentium tempora autem corporis sed ab ea neque
+          voluptatibus id voluptatum molestias nesciunt. Cum natus consectetur
+          suscipit consequatur, architecto excepturi optio. Et delectus fugit
+          minus dignissimos placeat amet?
         </p>
       </div>
-      <div className="steps-step3--container">
-        <h3 className="steps-step3--header">Step 1</h3>
-        <p className="steps-step3--description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ad
-          enim quia assumenda delectus alias doloremque. Placeat, ab, eligendi
-          optio hic saepe voluptatibus eaque, necessitatibus labore odit dolor
-          in adipisci assumenda harum maiores nisi animi aspernatur molestias
-          ipsam laboriosam tempore dolore! In, autem accusantium? Nam itaque
-          velit ipsum ad fugit!
+      <div className="steps-step--container">
+        <div className="steps-step-information">
+          <h3 className="steps-step--header">Step 1</h3>
+          <p className="steps-step--description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            ad enim quia assumenda delectus alias doloremque. Placeat, ab,
+            eligendi optio hic saepe voluptatibus eaque, necessitatibus labore
+            odit dolor in adipisci assumenda harum maiores nisi animi aspernatur
+            molestias ipsam laboriosam tempore dolore! In, autem accusantium?
+            Nam itaque velit ipsum ad fugit!
+          </p>
+        </div>
+        {/* 'insert image instead of <p>' */}
+        <p className="img-placeholder">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+          deleniti nemo ex necessitatibus libero veniam quos, in, amet iure, hic
+          unde corrupti praesentium tempora autem corporis sed ab ea neque
+          voluptatibus id voluptatum molestias nesciunt. Cum natus consectetur
+          suscipit consequatur, architecto excepturi optio. Et delectus fugit
+          minus dignissimos placeat amet?
         </p>
       </div>
     </div>
