65270187de179455764f8f584daf026b652510ec
BuraYu <burak.yueksel@hotmail.com>
2024-06-29 23:01:03 +0200

    add steps component

--- src/Components/Steps.jsx
+++ src/Components/Steps.jsx
@@ -3,8 +3,40 @@ import "./Steps.css";
 
 export default function Steps() {
   return (
-    <div>
-      <h1 className="steps">These are the steps</h1>
+    <div className="steps">
+      <div className="steps-step1--container">
+        <h3 className="steps-step1--header">Step 1</h3>
+        <p className="steps-step1--description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ad
+          enim quia assumenda delectus alias doloremque. Placeat, ab, eligendi
+          optio hic saepe voluptatibus eaque, necessitatibus labore odit dolor
+          in adipisci assumenda harum maiores nisi animi aspernatur molestias
+          ipsam laboriosam tempore dolore! In, autem accusantium? Nam itaque
+          velit ipsum ad fugit!
+        </p>
+      </div>
+      <div className="steps-step2--container">
+        <h3 className="steps-step2--header">Step 1</h3>
+        <p className="steps-step2--description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ad
+          enim quia assumenda delectus alias doloremque. Placeat, ab, eligendi
+          optio hic saepe voluptatibus eaque, necessitatibus labore odit dolor
+          in adipisci assumenda harum maiores nisi animi aspernatur molestias
+          ipsam laboriosam tempore dolore! In, autem accusantium? Nam itaque
+          velit ipsum ad fugit!
+        </p>
+      </div>
+      <div className="steps-step3--container">
+        <h3 className="steps-step3--header">Step 1</h3>
+        <p className="steps-step3--description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ad
+          enim quia assumenda delectus alias doloremque. Placeat, ab, eligendi
+          optio hic saepe voluptatibus eaque, necessitatibus labore odit dolor
+          in adipisci assumenda harum maiores nisi animi aspernatur molestias
+          ipsam laboriosam tempore dolore! In, autem accusantium? Nam itaque
+          velit ipsum ad fugit!
+        </p>
+      </div>
     </div>
   );
 }
