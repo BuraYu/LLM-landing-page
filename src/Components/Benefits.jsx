26d5749fc1224a837667686dd748aba781d4d1fd
BuraYu <burak.yueksel@hotmail.com>
2024-06-30 13:19:59 +0200

    add framer to carousel in Benefit Component

--- src/Components/Benefits.jsx
+++ src/Components/Benefits.jsx
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
 import "./Benefits.css";
 import benefits from "../data";
+import { motion } from "framer-motion";
 
 const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  // const [direction, setDirection] = useState(1);
 
   const nextBenefit = () => {
+    // setDirection(1);
     setCurrentIndex((prevIndex) => (prevIndex + 1) % benefits.length);
   };
 
   const prevBenefit = () => {
+    // setDirection(-1);
     setCurrentIndex(
       (prevIndex) => (prevIndex - 1 + benefits.length) % benefits.length
     );
+  };
+
+  const variants = {
+    enter: { opacity: 0 },
+    center: { opacity: 1, transition: { duration: 1 } },
+    exit: { opacity: 0 },
   };
 
   return (
@@ -20,7 +30,15 @@ const Carousel = () => {
       <button className="carousel-button" onClick={prevBenefit}>
         ←
       </button>
-      <div className="carousel-content">
+      <motion.div
+        className="carousel-content"
+        key={currentIndex}
+        variants={variants}
+        initial="enter"
+        animate="center"
+        exit="exit"
+        transition={{ duration: 0.5 }}
+      >
         <div className="carousel-details">
           <h2>{benefits[currentIndex].heading}</h2>
           {benefits[currentIndex].details.map((detail, index) => (
@@ -36,7 +54,7 @@ const Carousel = () => {
             </div>
           ))}
         </div>
-      </div>
+      </motion.div>
       <button className="carousel-button" onClick={nextBenefit}>
         →
       </button>
